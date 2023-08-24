4065a130b1b7ab216a4c7b8c11cbd508c3852eba
Vinícius Andrade <viniciusfuturo@outlook.com>
2023-08-24 16:45:51 -0300

    Alteracoes no readme e excecoes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Engenharia_de_Dados_Cotacao_Criptomoedas
+O mercado de criptomoedas tem experimentado um crescimento exponencial nos últimos anos, com uma variedade cada vez maior de ativos digitais disponíveis para compra, venda e negociação. Para acompanhar a volatilidade e as flutuações de preços desses ativos, surgiram diversos projetos e ferramentas de cotação, sendo o CoinMarketCap um dos mais renomados e utilizados, e que usaremos para cotação dos ativos escolhidos.
+
+# CoinMarketCap
+Conforme comentado, o CoinMarketCap é um site que rastreia e fornece informações sobre uma ampla gama de criptomoedas, incluindo seus preços em tempo real, capitalização de mercado, volume de negociação, suprimento circulante, histórico de preços e muito mais. Ele atua como uma plataforma de referência para investidores, traders, entusiastas de criptomoedas e qualquer pessoa interessada em acompanhar o mercado digital.
+
+# Projeto
+Conforme explicado, explanado e visando a coleta e organização dos dados relacionados às moedas para obtenção de insights e conclusão dessa tarefa:
+
+# 1) criei um processo ETL:
+    # E: Extração dos dados respectivos oriundos da fonte https://coinmarketcap.com/pt-br/, a partir do acréscimo da moeda na url e buscando, na classe específica, a classe que armazena os dados que queremos, sendo eles: Moeda, Sigla da Moeda, Preço, 1h%, Unidade, Data da Captura
+
+    # T: Posterior à coleta dos dados, e visando a geração de um relatório formal relacionado às cotações, elaborei uma lógica para coletar cada informação, tratei os dados, transformei em dicionário - pela facilidade de trabalhar como dataframe, e adicionei ao dataframe
+
+    # L: Logo após os tratamentos, fiz o carregamento em um arquivo csv, um arquivo já tratado, limpo e em conformidade ao que esperamos para facilidade de obtenção de insights.
+
+# 
--- cotacao.py
+++ cotacao.py
@@ -2,41 +2,48 @@ import pandas as pd
 import requests as rq
 from bs4 import BeautifulSoup as bs4
 import time
+import logging
+import re
 
 df = pd.DataFrame()
+data = time.strftime("%Y-%m-%d %H:%M:%S")
 coins = ['bitcoin', 'ethereum', 'tether', 'bnb', 'xrp', 'usd-coin', 'cardano']
+logging.basicConfig(filename="file.log", level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 for index, coin in enumerate(coins):
+
     try:
         html = rq.get(f'https://coinmarketcap.com/pt-br/currencies/{coin}/')
+        html.raise_for_status()
         soup = bs4(html.text, 'html.parser')
-    except rq.exceptions.RequestException as e:
-        print(f"Erro na requisição HTTP para {coin}: {e}")
-        continue
 
-    try:
-        aboutCoin = [item for item in soup.find('div', attrs={'class':'sc-16891c57-0 hqcKQB flexStart alignBaseline'}).text.split("\xa0") if item != '']
-    except AttributeError as e:
-        print(f"Erro ao fazer parsing do HTML para {coin}: {e}")
-        continue
-
-    try:
+        try:
+            aboutCoin = [item for item in soup.find('div', attrs={'class':'sc-16891c57-0 hqcKQB flexStart alignBaseline'}).text.split("\xa0") if item != '']
+            padrao = re.match(r'(\d+)([A-Za-z]+)', aboutCoin[-1][1:-1])
+            logging.info(f"Dados - Moeda: {coin}")
+        except AttributeError as attr_err:
+            logging.error(f"Erro de Atributo: {attr_err}")
+    
         dictionary = {
             'Moeda': coin.replace("-"," ").title(),
-            'Preco_Captura': float(aboutCoin[0].replace("R$","").replace(",","").replace(".","").replace(" ","")),
+            'Preco': float(aboutCoin[0].replace("R$","").replace(",","").replace(".","").replace(" ","")),
             'Sigla_Preco': 'BRL',
-            'Data_Captura': time.strftime("%Y-%m-%d %H:%M:%S"),
+            'Data_Captura': data.split(' ')[0],
+            'Hora_Captura': data.split(' ')[1],
             'Variacao': float(aboutCoin[1].replace("%","")),
-            'Periodicidade': aboutCoin[2].replace("(","").replace(")","")
+            'Periodo_Qtde': padrao.group(1),
+            'Periodo_Und': padrao.group(2)
         }
-    except (ValueError, IndexError) as e:
-        print(f"Erro na conversão de dados para {coin}: {e}")
-        continue
     
-    try:
-        df_temp = pd.DataFrame(dictionary, index=[index])
-        df = pd.concat([df, df_temp])
-    except pd.errors.MergeError as e:
-        print(f"Erro ao inserir dados no DataFrame para {coin}: {e}")
-        
-print(df)
+        df = pd.concat([df, pd.DataFrame(dictionary, index=[index])])
+    
+    except rq.exceptions.HTTPError as http_err:
+        logging.error(f"Erro HTTP: {http_err}")
+    except rq.exceptions.RequestException as req_err:
+        logging.error(f"Erro de Requisição: {req_err}")
+    except Exception as err:
+        logging.error(f"Erro Desconhecido: {err}")
+
+df.reset_index(inplace=True)
+df.drop('index', axis=1, inplace=True)
+df.to_csv(f"Cotação_{data.split(' ')[0]}.csv", sep=';', encoding='ISO-8859-1')
